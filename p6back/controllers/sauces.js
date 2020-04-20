4dc042448dbb04154860ca4c205972d005408487
DevonTheFloor <devonthefloor@free.fr>
2020-04-20 21:53:45 +0200

    liked ne fonctionne pas

--- p6back/controllers/sauces.js
+++ p6back/controllers/sauces.js
@@ -48,18 +48,45 @@ exports.like = (req,res,next)=>{
    console.log("LIKE in route");
   Sauce.find({_id: req.params.id})
     .then((response)=> {console.log(response);
-         console.log(req.body);}
-  )
+
+        var userLiked= req.params.userLiked;
+        var userDisliked= req.params.useDisliked; 
+        console.log(req.body);
+                        console.log("rekparUL : "+req.params.userLiked);
+
+      switch (req.body.like) {
+
+      case 1:
+        function liked() {
+          console.log("function liked");
+      if (userLiked.indexOf(req.body.userId) === -1) {
+        userLiked.push(req.body.userId);
+        console.log('Apres ajout du nouveau user : ' + userLiked);
+        } else if (userLiked.indexOf(req.body.userId) > -1) {
+        console.log(req.body.userId + 'A Déjà liké cette sauce.');
+          }
+       Sauce.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
+    .then(() => res.status(200).json({ message: 'Objet modifié !'}))
+    .catch(error => res.status(400).json({message:"sauce non trouvée"}));
+          console.log("fin liked");
+};
+
+      break;
+      case -1:
+      //liked(userDisLiked,:id);
+        console.log('like = -1 tu n\'aimes pas');
+      break;
+      case 0:
+               console.log("ZERO !!");
+        //zero(userID);
+      break;
+      default:
+        res.status(400).json({message:"la requête ne possède pas de valeur like"});
+    }
+  })
     .catch(error=> res.status(400).json({message:"sauce non trouvée"}));
 
   };
-  
-//  let params = new URLSearchParams(document.location.search);
-//console.log(params);
-//let id = params.get("like");
-//console.log('like= ' + like);
-  
-//{ usersLiked: [], usersDisliked: [], _id: 5e9cc3352366aa1fb4beb287 }
 
 
 
--- p6back/middleware/auth.js
+++ p6back/middleware/auth.js
@@ -4,15 +4,16 @@ module.exports = (req, res, next) => {
   try {
     const token = req.headers.authorization.split(' ')[1];
     const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET');
+    //Dial0go_s0pra_i_dÛe_m@ssimi_6temi_d-L_mondo
     const userId = decodedToken.userId;
     if (req.body.userId && req.body.userId !== userId) {
-      throw 'Invalid user ID';
+      throw 'Id utilisateur inconnu';
     } else {
       next();
     }
   } catch {
     res.status(401).json({
-      error: new Error('Invalid request!')
+      error: new Error("la requête n'a pas de jeton")
     });
   }
 };
